79b78db849bb35afb93b435268d647f3fc5b871d
salmondavid <61057915+trigerme@users.noreply.github.com>
2023-03-04 21:06:22 +0530

    y

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ burger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
   burger.classList.toggle('toggle');
 });
+
+//HEADER COLOR CHANGE When scroll
+  window.addEventListener('scroll', function() {
+    var header = document.querySelector('#header');
+    console.log(header); // check if header is correctly selected
+    var scrollPosition = window.scrollY;
+    if (scrollPosition > 1) {
+      header.style.backgroundColor = 'red';
+    } else {
+      header.style.backgroundColor = 'var(--body-color)';
+    }
+  });
